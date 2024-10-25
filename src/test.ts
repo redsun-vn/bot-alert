ccf1eb62a61b89809469f009d127cb1b58b09ef0
Lan Vu <ibmlenovo1996@gmail.com>
2024-10-25 10:20:25 +0700

    chore(env): remove example env

--- src/test.ts
+++ src/test.ts
@@ -2,8 +2,8 @@ import { LarkSuitAlert } from './utils/larksuit-bot';
 async function main(): Promise<void> {
     const timestamp = String(Math.floor(Date.now() / 1000) + 100);
 	const botLark = new LarkSuitAlert({
-        webHookUrl: 'https://open.larksuite.com/open-apis/bot/v2/hook/4d5756a5-f6ae-42aa-bd70-adca81754a42',
-        secretKey: 'PeJN1bdUHOllBjwyzh3qxc',
+        webHookUrl: '',
+        secretKey: '',
         timestamp: timestamp
     });    
     const res = await botLark.sendMessage('ApiGateway', 'Error', '');
